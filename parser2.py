c7979c114ec654cb2af2f21065f7ffe5824f2d9a
Jarvis <jorge.gomzar@gmail.com>
2018-03-03 19:49:43 +0100

    Segunda versión para el parseador de Soundcloud, esta vez desarrollado en Python

---
+++ parser2.py
@@ -0,0 +1,44 @@
+import os
+from colorama import *
+init(autoreset=True)
+
+print(Back.YELLOW + Fore.WHITE + '┌' + format('','─^78') + '┐')
+print( Back.YELLOW + Fore.WHITE + '│' + format('Bienvenid@ al Parser2.0'.title(),'^78') + '│')
+print(Back.YELLOW + Fore.WHITE + '└' + format('','─^78') + '┘')
+
+print(Fore.BLUE+'\n[+] Pega aquí la ruta de los archivos que quieres parsear: ')
+ruta = input()
+listado = []
+nuevoListado = []
+
+# Exporto a un archivo los nombres de los ficheros y los guardo linea a linea en una cadena
+os.system('dir /b "' + ruta + '" > archivos.txt')
+archivo = open('archivos.txt', 'r')
+for x in range(1, 100):
+	linea = archivo.readline()
+	if linea == '':
+		break
+	listado.insert((x-1), linea.rstrip('\n'))
+archivo.close()
+os.system('del archivos.txt')
+numero = len(listado)
+print(Fore.BLUE+'\n[+] Los archivos tienen actualmente los siguientes nombres:')
+for item in listado:
+	print('-',item)
+
+# Guardo en una nueva lista los nombres parseados	
+print(Fore.BLUE+'\n[+] Los nombres de los archivos serán cambiados por los que siguen respectivamente:')
+for x in range(0, len(listado)):
+	nuevaLinea = ''
+	linea = listado[x]
+	for i in linea:
+		if i == '_':
+			break
+		nuevaLinea = (nuevaLinea + i)
+	print('+', nuevaLinea)
+	nuevoListado.insert((x), nuevaLinea)
+
+# Cambio de nombre a los ficheros por los nuevos nombres en nuevoListado
+for x in range(0, len(nuevoListado)):
+	os.system('rename "' + ruta + '\\' + listado[x] + '" ' + nuevoListado[x])
+print(Fore.GREEN + '\n[+] Todos los ficheros han sido cambiados de nombre con éxito.')
