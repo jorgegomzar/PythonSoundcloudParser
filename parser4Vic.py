77b9d6d614339abc1e504abd6aa5683f87378ef3
jorgegomzar <jorge.gomzar@gmail.com>
2018-09-21 23:01:41 +0200

    Experimental function added

--- parser4Vic.py
+++ parser4Vic.py
@@ -11,32 +11,51 @@ def listar(ruta, listado):
 	for item in allFiles:
 		listado.append(item)
 
-def cambiarNombres(listado, nuevoListado):
+# def cambiarNombres(listado, nuevoListado):
+# 	"""Guarda en una nueva lista los nombres parseados"""
+# 	for item in listado:
+# 		nuevaLinea = ''
+# 		posible = 0
+# 		spam = ''
+# 		for i in item:
+# 			if i == '_':
+# 				break
+# 			if i == '(' or i == '[':
+# 				posible = 1
+# 			if posible == 1:
+# 				spam = spam + i
+# 			nuevaLinea = (nuevaLinea + i)
+# 			if i == ')' or i == ']':
+# 				if 'FREE' in spam.upper() or 'OUT NOW' in spam.upper():
+# 					nuevaLinea = nuevaLinea.strip(spam)
+# 				posible = 0
+# 				spam = ''
+# 	#########################################################################
+# 		# Estas dos nuevas lineas evitan duplicados de '.mp3' en el nombre
+# 		nuevaLinea = nuevaLinea.rstrip('.mp3')
+# 		nuevaLinea = nuevaLinea + '.mp3'
+# 	#########################################################################
+# 		print('+', nuevaLinea)
+# 		nuevoListado.append(nuevaLinea)
+
+def cambiarNombres(listado, nuevoListado): # Nueva función experimental más corta
 	"""Guarda en una nueva lista los nombres parseados"""
 	for item in listado:
-		nuevaLinea = ''
+		linea = item.split(' - ')
 		posible = 0
-		spam = ''
-		for i in item:
-			if i == '_':
-				break
-			if i == '(' or i == '[':
+		contador = 0
+		garbage = ['_']
+		for i in linea[0]:
+			if posible == 1 and i.isdigit():
+				garbage.append(i)
+				contador = contador + 1
+			elif contador == 9:
+				linea[0] = linea[0].rstrip(''.join(garbage))
+			elif i == '_':
 				posible = 1
-			if posible == 1:
-				spam = spam + i
-			nuevaLinea = (nuevaLinea + i)
-			if i == ')' or i == ']':
-				if 'FREE' in spam.upper() or 'OUT NOW' in spam.upper():
-					nuevaLinea = nuevaLinea.strip(spam)
-				posible = 0
-				spam = ''
-	#########################################################################
-		# Estas dos nuevas lineas evitan duplicados de '.mp3' en el nombre
-		nuevaLinea = nuevaLinea.rstrip('.mp3')
-		nuevaLinea = nuevaLinea + '.mp3'
-	#########################################################################
-		print('+', nuevaLinea)
-		nuevoListado.append(nuevaLinea)
+		nuevoListado.append(' - '.join(linea))
+	for item in nuevoListado:
+		print('+', item)
 
 def continuar():
 	"""Pregunta al usuario si quiere realizar los cambios o abandonar la operación"""
@@ -65,8 +84,6 @@ print(Fore.BLUE+'\n[+] Los nombres de los archivos serán cambiados por los que 
 cambiarNombres(listado, nuevoListado)
 
 continuar()
-for i, item in enumerate(nuevoListado, start=1):
-	# os.system('rename "' + ruta + '\\' + listado[i] + '" "' + item + '"')
+for i, item in enumerate(nuevoListado, start=0):
 	os.rename(ruta + '\\' + listado[i], ruta + '\\' + item)
-	# shutil.move('ruta'+'\\'+listado[i], ruta+'\\'+item) # Nuevo codigo experimental
 print(Fore.GREEN + '\n[+] Todos los ficheros han sido cambiados de nombre con éxito.')
